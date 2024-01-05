f8727f491d295355229d0fdd7c985bd46d432ffd
HubPavKul1 <kulpv@mail.ru>
2024-01-05 12:26:43 +0300

    add add company page

--- vetwebapi/vetwebapi/frontend/pages/company_page.py
+++ vetwebapi/vetwebapi/frontend/pages/company_page.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Request, Depends, Form
-from fastapi.responses import HTMLResponse
+from typing import Annotated
+from fastapi.responses import HTMLResponse, RedirectResponse
 from vetwebapi.core.settings import settings
 from vetwebapi.api_v1.company.views import get_companies, create_company_route
-from vetwebapi.api_v1.company.schemas import Companies
+from vetwebapi.api_v1.company.schemas import Companies, CompanyIn
 from vetwebapi.core.models import Company
 
 
@@ -22,6 +23,10 @@ async def get_add_company_form(request: Request):
 
 
 @router.post("/add_company", response_class=HTMLResponse)
-async def post_add_company_form(request: Request, full_name: str = Form(...), short_name: str = Form(...)):
+async def post_add_company_form(request: Request, full_name: Annotated[str, Form(...)], short_name: Annotated[str, Form(...)]):
+    new_company_schema = CompanyIn(full_name=full_name, short_name=short_name)
+    result = create_company_route(body=new_company_schema)
+    if result["result"] == True:
+        return RedirectResponse("/pages/companies")
     
-    return settings.templates.TemplateResponse("companies/add_company.html", {"request": request, "full_name": full_name, "short_name": short_name})
+    return settings.templates.TemplateResponse("companies/add_company.html", {"request": request})
