38c87f0036cbbe1796ccd8a2e915084be5ddd232
HubPavKul1 <kulpv@mail.ru>
2024-01-06 18:46:09 +0300

    add company_detail

--- vetwebapi/vetwebapi/frontend/pages/company_page.py
+++ vetwebapi/vetwebapi/frontend/pages/company_page.py
@@ -5,8 +5,8 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from vetwebapi.core.settings import settings
 from vetwebapi.core.database import db_manager
 from vetwebapi.api_v1.company import crud
-from vetwebapi.api_v1.company.views import get_companies, create_company_route
-from vetwebapi.api_v1.company.schemas import Companies, CompanyIn
+from vetwebapi.api_v1.company.views import get_companies, get_company_detail
+from vetwebapi.api_v1.company.schemas import Companies, CompanyIn, CompanyDetail
 from vetwebapi.core.models import Company
 
 
@@ -19,6 +19,7 @@ async def companies(request: Request, companies: Companies = Depends(get_compani
         "/companies/companies.html", {"request": request, "companies": companies.companies}
     )
 
+
 @router.get("/add_new_company", response_class=HTMLResponse)
 async def add_new_company(request: Request):
     return settings.templates.TemplateResponse("companies/add_company.html", {"request": request})
@@ -26,15 +27,20 @@ async def add_new_company(request: Request):
 
 @router.post("/add_company")
 async def add_company(
-    request: Request, 
-    full_name: Annotated[str, Form(...)], 
-    short_name: Annotated[str, Form(...)], 
+    request: Request,
+    full_name: Annotated[str, Form(...)],
+    short_name: Annotated[str, Form(...)],
     session: AsyncSession = Depends(db_manager.scope_session_dependency),
-    ):
-    
+):
     new_company_schema = CompanyIn(full_name=full_name, short_name=short_name)
     new_company = await crud.create_company(session=session, body=new_company_schema)
     print(new_company)
-    
+
     return RedirectResponse(url="/pages/companies", status_code=302)
 
+
+@router.get("/company_detail/{company_id}", response_class=HTMLResponse)
+async def company_detail(request: Request, company: CompanyDetail = Depends(get_company_detail)):
+    return settings.templates.TemplateResponse(
+        "/companies/company_detail.html", {"request": request, "company": company}
+    )
