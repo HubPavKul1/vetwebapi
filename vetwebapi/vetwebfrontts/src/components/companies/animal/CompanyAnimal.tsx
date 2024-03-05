00ea31bd8d88e71429d24414afa5a398bfb74430
HubPavKul1 <kulpv@mail.ru>
2024-03-05 12:30:05 +0300

    add react icons

--- vetwebapi/vetwebfrontts/src/components/companies/animal/CompanyAnimal.tsx
+++ vetwebapi/vetwebfrontts/src/components/companies/animal/CompanyAnimal.tsx
@@ -2,7 +2,8 @@ import { IAnimal } from "../../../interfaces/AnimalInterfaces";
 import { useParams } from "react-router-dom";
 import { useMutation, useQueryClient } from "react-query";
 import { AnimalService } from "../company.service";
-
+import { BsFillTrash3Fill } from "react-icons/bs";
+import { BsPencilSquare } from "react-icons/bs";
 
 interface CompanyAnimalProps {
     animal: IAnimal;
@@ -32,31 +33,14 @@ export function CompanyAnimal({animal}: CompanyAnimalProps) {
 
  
     return(
-        
             <tr key={animal.id}>
                 <td>{animal.species}</td>
                 <td>{animal.gender}</td>
                 <td>{animal.date_of_birth}</td>
                 <td>{animal.nickname}</td>
                 <td>{animal.identification}</td>
-                <td>
-                    <div className="btn-group btn-group-sm" role="group">
-                        <a
-                            href="#"
-                            className="btn btn-warning btn-sm"
-                            
-                        >
-                            ред
-                        </a>
-                        <a
-                            className="btn btn-danger btn-sm"
-                            href="#"
-                            onClick={deleteAnimal}
-                        >
-                            х
-                        </a>
-                    </div>
-                </td>
+                <td><BsPencilSquare className="edit-icon"/></td>
+                <td><BsFillTrash3Fill className="delete-icon" onClick={deleteAnimal}/></td>
             </tr>
         )                
 
