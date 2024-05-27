fbd40dc3b40bc587b526fca1f67f84130c51ecac
HubPavKul1 <kulpv@mail.ru>
2024-05-27 22:52:58 +0300

    refact add animals to vetwork

--- vetwebapi/vetwebfrontts/src/components/vetWorks/AddAnimalsToVetWorkForm/AddAnimalsToVetWorkForm.tsx
+++ vetwebapi/vetwebfrontts/src/components/vetWorks/AddAnimalsToVetWorkForm/AddAnimalsToVetWorkForm.tsx
@@ -1,163 +1,72 @@
-import { useForm, FormProvider, SubmitHandler, RegisterOptions, Controller, useFieldArray } from "react-hook-form"
-import { useMutation, useQuery, useQueryClient } from "react-query";
-import { IAnimal, IAnimalCreate } from "../../../interfaces/AnimalInterfaces";
+
+import { useQuery } from "react-query";
 import { CustomButton } from "../../button/CustomButton";
-import { Input } from "../../Input";
-import { FormInputProps } from "../../../interfaces/FormInterface";
-import { fieldRequiredMessage, maxLenErrorMessage, minLenErrorMessage } from "../../ErrorMessages";
 import { useParams } from "react-router-dom";
 import { AppService } from "../../../app.service";
-import { CompanySelect } from "../CompanySelect";
-import { IAnimalInVetworkIn, IAnimalsInVetworkIn } from "../../../interfaces/VetWorkInterfaces";
+
+
 import { ICompanyDetail } from "../../../interfaces/CompanyInterfaces";
 import { Container } from "react-bootstrap";
 
-import styles from "./AddAnimalsToVetWorkForm.module.scss"
+import styles from "./AddAnimalsToVetWorkForm.module.scss";
+import { AnimalFormItem } from "./AnimalFormItem";
 import { useState } from "react";
-
+import { IAnimalInVetworkIn } from "../../../interfaces/VetWorkInterfaces";
 
 interface AddAnimalsToVetWorkFormProps {
-    companyId: string;
-    setAnimals: CallableFunction;
+  companyId: string;
+  setAnimals: CallableFunction;
 }
 
 interface CompanyData {
-    data?: ICompanyDetail;
-    isLoading: boolean;
-  }
+  data?: ICompanyDetail;
+  isLoading: boolean;
+}
 
-export function AddAnimalsToVetWorkForm({companyId, setAnimals}: AddAnimalsToVetWorkFormProps) {
+export function AddAnimalsToVetWorkForm({
+  companyId,
+  setAnimals,
+}: AddAnimalsToVetWorkFormProps) {
 
-    const [animalsData, setAnimalsData] = useState<IAnimalInVetworkIn[]>()
-
-    const {id} = useParams()
-    const url = `/api/vetwork/${id}/animals/`
-
-    const companyUrl = `/api/companies/${companyId}`;
+    const [animalsData, setAnimalsData] = useState<IAnimalInVetworkIn[]>([])
 
 
-    const { isLoading, data }: CompanyData = useQuery(['vetworkCompany', id], () => AppService.get(companyUrl), {
-      enabled: !!id
+
+
+  const { id } = useParams();
+
+  const companyUrl = `/api/companies/${companyId}`;
+
+  const { isLoading, data }: CompanyData = useQuery(
+    ["vetworkCompany", id],
+    () => AppService.get(companyUrl),
+    {
+      enabled: !!id,
     }
-    );
-
- 
-    const methods = useForm<IAnimalsInVetworkIn>({
-        mode: "onChange",
-        defaultValues: {
-            animals:[{animal_id: 0, dosage: 0, is_positive: false}]
-
-        }
-    })
-
-    // const { register, reset, handleSubmit, formState: {errors} } = useForm<IAnimalInVetworkIn>({
-    //     mode: "onChange",
-    // })
-
-    const { control, register, reset, handleSubmit, formState: { errors } } = methods
-    const queryClient = useQueryClient()
-
-    const { fields } = useFieldArray({
-        name: "animals",
-        control: control,
-        shouldUnregister: true
-      })
-
-    const { mutate } = useMutation(["add animals"], {
-        mutationFn: (data: IAnimalsInVetworkIn) => AppService.createItem(url, data),
-        onSuccess: () => {
-            alert("Животное успешно добавлено!")
-            queryClient.invalidateQueries(["vaccination", id])
-            reset()
-        }
-    },
-    );
+  );
 
 
-    const addAnimals: SubmitHandler<IAnimalsInVetworkIn> = (data) => {
-        console.log("DATA>>>>", data)
-        console.log()
-        
-        // mutate(data)
+  if (isLoading || !data) return <p>Загрузка ...</p>;
 
-    }
+  if (!data.animals) return;
 
- 
+  const animals = data.animals;
 
-    if(isLoading || !data) return <p>Загрузка ...</p>;
+  console.log("StateDATA>>>", animalsData)
 
-    if(!data.animals) return;
+  return (
+    <Container className={styles.wrapper}>
+      <CustomButton
+        className="btn-upload"
+        title="Назад"
+        onClick={() => setAnimals(false)}
+      />
 
-    const animals = data.animals
-
-
-    return (
-        <Container>
-            <CustomButton className="btn-upload" title= "Назад" onClick={() => setAnimals(false)} />
-            <FormProvider {...methods}>
-            <form onSubmit={handleSubmit(addAnimals)}>
-            {fields.map((animal, index) => (
-            <Container className={styles.formWrap} key={animal.id}>
-                 <Controller 
-                    name={`animals.${index}.id`}
-                    control={control}
-                    defaultValue={animals[index].id}
-                    render={({ field: { value, onChange } }) =>
-                    <div className="form-group">
-                        <label htmlFor={animal.id.toString()}>
-                            {animals[index].nickname} {animals[index].id}
-                        <input
-                            type="number"
-                            id={animal.id.toString()}
-                            value={value}
-                            onChange={onChange}
-                                        
-                        />
-                        </label>
-                    </div>
-                            
-                    }
-                />
-                <Controller 
-                    name="dosage"
-                    control={control}
-                    render={({ field: { value, onChange } }) =>
-                    <div className="form-group">
-                        <label htmlFor="dosage">
-                            Дозировка
-                        <input
-                            name="dosage"
-                            type="number"
-                            id="dosage"
-                            value={value}
-                            onChange={onChange}
-                                        
-                        />
-                        </label>
-                    </div>
-                            
-                    }
-                    />   
-                    </Container>
-                    
-                    
-                ))}
-            
-                <div className="form-group">
-                    <CustomButton
-                        className="btn-submit"
-                        disabled={false}
-                        title="Зарегистрировать"
-                    />
-                </div>
-
-            </form>
-                
-
-            </FormProvider>
-
+      {animals.map((animal) => (
+        <Container key={animal.id}>
+          <AnimalFormItem animal={animal} setAnimalsData={setAnimalsData} animalsData={animalsData}/>
         </Container>
-        
-
-    )
+      ))}
+    </Container>
+  );
 }
---
+++ vetwebapi/vetwebfrontts/src/components/vetWorks/AddAnimalsToVetWorkForm/AnimalFormItem.tsx
@@ -0,0 +1,99 @@
+import { useMutation, useQueryClient } from "react-query";
+import { IAnimal } from "../../../interfaces/AnimalInterfaces";
+import { IAnimalInVetworkIn } from "../../../interfaces/VetWorkInterfaces";
+import { CustomButton } from "../../button/CustomButton";
+import { AppService } from "../../../app.service";
+import { useParams } from "react-router-dom";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import styles from "./AddAnimalsToVetWorkForm.module.scss";
+import { Col, Row } from "react-bootstrap";
+
+interface AnimalFormItemProps {
+  animal: IAnimal;
+  setAnimalsData: CallableFunction;
+  animalsData: IAnimalInVetworkIn[];
+}
+
+export function AnimalFormItem({
+  animal,
+  setAnimalsData,
+  animalsData,
+}: AnimalFormItemProps) {
+  const { id } = useParams();
+
+  const url = `/api/vetwork/${id}/animals/`;
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IAnimalInVetworkIn>({
+    mode: "onChange",
+  });
+
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation(["add animals"], {
+    mutationFn: (data: IAnimalInVetworkIn) => AppService.createItem(url, data),
+    onSuccess: () => {
+      alert("Животное успешно добавлено!");
+      queryClient.invalidateQueries(["vaccination", id]);
+      reset();
+    },
+  });
+
+  const addAnimal: SubmitHandler<IAnimalInVetworkIn> = (data) => {
+    console.log("DATA>>>", data);
+    setAnimalsData([...animalsData, data]);
+    // mutate(data)
+  };
+
+  return (
+    <form onSubmit={handleSubmit(addAnimal)}>
+      <Row className={styles.formWrap}>
+        <Col>{animal.species}</Col>
+        <Col>
+          <label htmlFor="animal_id">
+            {animal.nickname}
+            <input
+              type="hidden"
+              defaultValue={animal.id}
+              id="animal_id"
+              {...register("animal_id")}
+            />
+          </label>
+        </Col>
+        <Col>
+          <input
+            type="number"
+            step="any"
+            id="dosage"
+            placeholder="Доза"
+            {...register("dosage")}
+          />
+        </Col>
+        <Col>
+          <label htmlFor="is_positive">
+            Положительная реакция
+            <input
+              type="checkbox"
+              id="is_positive"
+              {...register("is_positive")}
+            />
+          </label>
+        </Col>
+
+        <Col>
+          <CustomButton
+            className="btn-submit"
+            disabled={false}
+            title="Добавить"
+          />
+        </Col>
+        <Col></Col>
+      </Row>
+    </form>
+  );
+}
