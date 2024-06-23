9fcbe77a27fa2c698bec76e9e8258e8b704cd72b
HubPavKul1 <kulpv@mail.ru>
2024-06-23 21:27:08 +0300

    do report main page

---
+++ vetwebapi/vetwebfrontts/src/components/drugs/drugReports/reportMenuItems/DrugMovementBetweenDateRange.tsx
@@ -0,0 +1,9 @@
+import { CreateItem } from "../../../CreateItem";
+
+export default function DrugMovementBetweenDateRange() {
+  return (
+    <CreateItem btnTitle="Движение биопрепаратов за период времени">
+      <div>Форма</div>
+    </CreateItem>
+  );
+}
