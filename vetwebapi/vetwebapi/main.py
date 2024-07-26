c2e3e50eea4788cf43f15e66fc738c7f6262967d
HubPavKul1 <kulpv@mail.ru>
2024-07-26 16:21:44 +0300

    dockerizing

--- vetwebapi/vetwebapi/main.py
+++ vetwebapi/vetwebapi/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http//localhost:5173",
-        "http//192.168.99.101"
+        "http//192.168.99.101:3000"
         ],
     allow_credentials=True,
     allow_methods=["*"],
