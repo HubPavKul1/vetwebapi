3b198d3e0501e71e7998c3556de8acee571422c4
HubPavKul1 <kulpv@mail.ru>
2024-07-26 22:26:48 +0300

    dockerize front

--- vetwebapi/vetwebfrontts/vite.config.ts
+++ vetwebapi/vetwebfrontts/vite.config.ts
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    watch: {
+      usePolling: true
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
     proxy: {
-      "/api": "http://192.168.99.101:8000"
+      "/api": "http://app:8000"
     }
   },
   plugins: [react()],
