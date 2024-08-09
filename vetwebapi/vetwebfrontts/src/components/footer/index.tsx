55cb784238d37cf2ea6be5c2900418899e3aa199
HubPavKul1 <kulpv@mail.ru>
2024-08-09 22:33:53 +0300

    settings vscode

--- vetwebapi/vetwebfrontts/src/components/footer/index.tsx
+++ vetwebapi/vetwebfrontts/src/components/footer/index.tsx
@@ -1,21 +1,16 @@
-import { Container } from "react-bootstrap"
+import { Container } from "react-bootstrap";
 import { NavList } from "../header/navList/NavList";
 
-import styles from "./Footer.module.scss"
-
-
+import styles from "./Footer.module.scss";
 
 export function Footer() {
-    
-    return (
-        <footer className="mt-auto">
-            <Container className={styles.footerWrap}>
-                <Container className={styles.footerContainer}>
-                    <NavList/>
-                </Container>
-             </Container>
-
-        </footer>
-       
-    )
+  return (
+    <footer className="mt-auto">
+      <Container className={styles.footerWrap}>
+        <Container className={styles.footerContainer}>
+          <NavList />
+        </Container>
+      </Container>
+    </footer>
+  );
 }
