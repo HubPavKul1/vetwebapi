ee1067302c2037143123f4e1e63f75d54616060f
HubPavKul1 <kulpv@mail.ru>
2024-11-05 15:30:55 +0300

    add tests

--- vetwebapi/vetwebapi/tests/test_company_api.py
+++ vetwebapi/vetwebapi/tests/test_company_api.py
@@ -1,7 +1,5 @@
 from httpx import AsyncClient
 from .conftest import test_db_manager
-from core.database import db_url
-
 
 
 async def test_create_company_route(ac: AsyncClient):
@@ -10,13 +8,41 @@ async def test_create_company_route(ac: AsyncClient):
     assert response.status_code == 201
 
 
-async def test_create_test_companies_route(ac: AsyncClient):
-    response = await ac.post("/api/companies/test")
+async def test_create_clinic_route(ac: AsyncClient):
+    company_data = {"full_name": "VetClinic1", "short_name": "Vet1"}
+    response = await ac.post("/api/companies/vets", json=company_data)
     assert response.status_code == 201
+
+
+async def test_create_lab_route(ac: AsyncClient):
+    company_data = {"full_name": "VetLab1", "short_name": "Lab1"}
+    response = await ac.post("/api/companies/labs", json=company_data)
+    assert response.status_code == 201
+
+
+# async def test_create_test_companies_route(ac: AsyncClient):
+#     response = await ac.post("/api/companies/test")
+#     assert response.status_code == 201
 
 
 async def test_get_companies(ac: AsyncClient):
     response = await ac.get("/api/companies/")
     print("Response>>>>>>>", response.json())
-    print("DB_URL+++++", db_url)
     assert response.status_code == 200
+
+
+async def test_get_clinics(ac: AsyncClient):
+    response = await ac.get("/api/companies/vets")
+    print("Response>>>>>>>", response.json())
+    assert response.status_code == 200
+
+
+async def test_get_labs(ac: AsyncClient):
+    response = await ac.get("/api/companies/labs")
+    print("Response>>>>>>>", response.json())
+    assert response.status_code == 200
+
+
+# async def test_delete_company(ac: AsyncClient):
+#     response = await ac.delete("/api/companies/1/")
+#     assert response.status_code == 202
