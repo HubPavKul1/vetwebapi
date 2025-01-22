bd044d964610a82fbdec352c47e441feeb5e0159
HubPavKul1 <kulpv@mail.ru>
2025-01-22 21:06:22 +0300

    refact frontend

--- vetwebapi/vetwebfrontts/src/entities/vetWorkReport/ui/CompanyVetWorkReportItem.tsx
+++ vetwebapi/vetwebfrontts/src/entities/vetWorkReport/ui/CompanyVetWorkReportItem.tsx
@@ -1,4 +1,5 @@
 import { ICompanyVetWorksReport } from "features/vetWork/models/interfaces";
+import { convertDateString } from "shared/helpers";
 
 interface CompanyVetWorkReportItemProps {
   data: ICompanyVetWorksReport;
@@ -9,10 +10,12 @@ export function CompanyVetWorkReportItem({
   data,
   rowNum,
 }: CompanyVetWorkReportItemProps) {
+
+  const vetworkDate = convertDateString(data.vetwork_date).shortDate
   return (
     <tr key={rowNum} className="border-bottom border-black align-middle">
       <td>{rowNum}</td>
-      <td>{data.vetwork_date}</td>
+      <td>{vetworkDate}</td>
       <td>{data.work_type}</td>
       <td>{data.disease}</td>
       <td>{data.animals_count}</td>
